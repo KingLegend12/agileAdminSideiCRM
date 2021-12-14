43a4b5b7da55e4784d47c1160fabb4b103f0eb0a
DrissJD <85832488+KingLegend12@users.noreply.github.com>
2021-12-14 19:04:55 +0100

    Delete src directory

--- src/layout/partial/Footer.comp.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <div className="text-center copy-right">
-      &copy; iCRM all right reserved -2021
-    </div>
-  );
-};
--- src/utils/validation.js
+++
@@ -1,3 +0,0 @@
-export const shortText = (str) => {
-  return str >= 3 && str <= 100;
-};
